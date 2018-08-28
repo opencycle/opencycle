37b30da9d0cc8050e65178491539cd6dcac92931
threesquared <ben@3sq.re>
2018-08-28 16:24:23 +0100

    More user tests

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,9 +2,8 @@
 
 namespace Tests\Feature;
 
+use Opencycle\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
@@ -23,5 +22,38 @@ class UserTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
+    }
+
+    /**
+     * Test a user can view their profile.
+     *
+     * @return void
+     */
+    public function testUserCanViewProfile()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('profile'));
+
+        $response->assertSee($user->username);
+    }
+
+    /**
+     * Test a user can edit their profile.
+     *
+     * @return void
+     */
+    public function testUserCanEditProfile()
+    {
+        $user = factory(User::class)->create();
+
+        $newData = [
+            'username' => $this->faker->userName,
+            'email' => $this->faker->email,
+        ];
+
+        $this->actingAs($user)->patch(route('users.update', $user), $newData);
+
+        $this->assertDatabaseHas('users', $newData);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,29 +2,13 @@
 
 namespace Tests;
 
-use Faker\Factory;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-
-    /**
-     * Instance of Faker.
-     *
-     * @var Generator
-     */
-    protected $faker;
-
-    /**
-     * Set up tests.
-     *
-     * @return void
-     */
-    public function setUp()
-    {
-        parent::setUp();
-
-        $this->faker = Factory::create(config('app.locale'));
-    }
+    use CreatesApplication,
+        WithFaker,
+        RefreshDatabase;
 }
