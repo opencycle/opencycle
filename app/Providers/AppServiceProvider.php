96f20692e9bddba92a7d85a183a8bad1d3b95669
threesquared <ben@3sq.re>
2018-09-02 15:28:21 +0100

    Update length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(767);
+        Schema::defaultStringLength(191);
     }
 
     /**
