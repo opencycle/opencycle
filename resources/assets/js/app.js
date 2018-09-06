2c9a4b8078fe677b9b11fd6c0e2faf645a0162f3
threesquared <ben@3sq.re>
2018-09-06 11:21:47 +0100

    Update JS

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,2 +1,16 @@
+import 'bootstrap.native/dist/bootstrap-native-v4';
+import Uppy from '@uppy/core';
+import Dashboard from '@uppy/dashboard';
+import XHRUpload from '@uppy/xhr-upload';
 
+const uppy = Uppy()
+    .use(Dashboard, {
+        trigger: '#select-files'
+    })
+    .use(XHRUpload, {
+        endpoint: '/posts/image'
+    })
 
+uppy.on('complete', (result) => {
+    console.log(`Upload complete! We’ve uploaded these files: ${result.successful}`)
+})
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,10 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Opencycle') }}</title>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="https://transloadit.edgly.net/releases/uppy/v0.27.3/dist/uppy.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="/favicon.png" />
 </head>
 <body>
