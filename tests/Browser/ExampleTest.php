ecd1dafedc10bf86c661a40ee7412de2e61287ac
threesquared <ben@3sq.re>
2018-11-01 17:54:40 +0000

    Dusk

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Opencycle');
+        });
+    }
+}
