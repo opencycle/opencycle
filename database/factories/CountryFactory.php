062d80d7723ad4ab197d730a79f915ed338b4272
Ben <ben.speakman@peakon.com>
2019-09-28 10:27:05 +0100

    Selectively seed countries for test data

--- database/factories/CountryFactory.php
+++ database/factories/CountryFactory.php
@@ -4,7 +4,41 @@ use Faker\Generator as Faker;
 use Opencycle\Country;
 
 $factory->define(Country::class, function (Faker $faker) {
-    $country = Countries::random();
+    $list = [
+        'AR',
+        'AU',
+        'AT',
+        'BE',
+        'BR',
+        'CM',
+        'CA',
+        'CN',
+        'CO',
+        'HR',
+        'CU',
+        'CY',
+        'EC',
+        'ET',
+        'DK',
+        'FI',
+        'FR',
+        'DE',
+        'GR',
+        'GT',
+        'HU',
+        'IE',
+        'JP',
+        'LV',
+        'NZ',
+        'NO',
+        'SE',
+        'TN',
+        'GB',
+        'US',
+        'ZM',
+    ];
+
+    $country = Countries::where('cca2', collect($list)->random())->first();
 
     return [
         'name' => $country->name->common,
